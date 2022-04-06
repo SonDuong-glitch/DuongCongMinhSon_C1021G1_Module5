57ae73d49bdfaf26baf1e638313422e370b7d6d1
SonDuong-glitch <83948475+SonDuong-glitch@users.noreply.github.com>
2022-04-06 14:18:21 +0700

    push

--- case-sutdy/src/app/facility/list-facility/list-facility.component.ts
+++ case-sutdy/src/app/facility/list-facility/list-facility.component.ts
@@ -6,7 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-facility.component.css']
 })
 export class ListFacilityComponent implements OnInit {
-
+  listFacility = [
+    { img: 'https://furamavietnam.com/wp-content/uploads/2018/03/Vietnam_Danang_Furama_Ocean-Suite-Feature-370x239.jpg',
+      name: 'Ocean Suite'
+    },
+    { img: 'https://furamavietnam.com/wp-content/uploads/2018/03/Vietnam_Danang_Furama_Ocean-Studio-Suite-F-370x239.jpg',
+      name: 'OCEAN STUDIO SUITE'
+    }, { img: 'https://furamavietnam.com/wp-content/uploads/2018/03/Vietnam_Danang_Furama_Ocean-Deluxe-double-bed-F-370x239.jpg',
+      name: 'OCEAN DELUXE'
+    },
+    { img: 'https://furamavietnam.com/wp-content/uploads/2018/03/Vietnam_Danang_Furama_Lagoon-Superior-twin-bed-F-370x239.jpg',
+      name: 'LAGOON SUPERIOR'
+    },
+    { img: 'https://furamavietnam.com/wp-content/uploads/2018/03/Vietnam_Danang_Furama_Garden-Superior-TwinBed-1-F-370x239.jpg',
+      name: 'GARDEN SUPERIOR'
+    },
+    { img: 'https://furamavietnam.com/wp-content/uploads/2018/08/Vietnam_Danang_Furama_Garden-Deluxe-twin-bed-F-370x239.jpg',
+      name: 'GARDEN DELUXE'
+    },
+    { img: 'https://furamavietnam.com/wp-content/uploads/2018/08/Vietnam_Danang_Furama_Garden-Deluxe-twin-bed-F-370x239.jpg',
+      name: 'PRESIDENTIAL SUITE'
+    },
+    { img: 'https://furamavietnam.com/wp-content/uploads/2018/08/Vietnam_Danang_Furama_Villas_Beach_Pool_Villas-_Exterior-1-F-370x239.jpg',
+      name: 'BEACH FRONT VILLAS'
+    }, { img: 'https://furamavietnam.com/wp-content/uploads/2018/08/Vietnam_Danang_Furama_Villas_Pool_Villas-F-370x239.jpg',
+      name: 'POOL VILLAS'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
